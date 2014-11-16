11af69dd5297f0920cf9949b4cec7c23ab652715
Vishal Gupta <vishal.vgu+github@gmail.com>
2014-11-17 00:48:23 +0530

    Utility stuff

--- source/server.py
+++ source/server.py
@@ -1,7 +1,7 @@
 from gevent import pywsgi
 
 def hello_world(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
+    start_response('200 OK', [('Content-Type', 'text/html'),("Access-Control-Allow-Origin", "*")])
     yield '<b>Hello world!</b>\n'
 
 server = pywsgi.WSGIServer(
