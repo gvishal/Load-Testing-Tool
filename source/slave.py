b7825bfb15fde2417692bb54ef15841693067b27
KavyaNerella <kavya.nerella@students.iiit.ac.in>
2014-11-18 17:18:04 +0530

    status added to slave

--- source/slave.py
+++ source/slave.py
@@ -16,7 +16,7 @@ api = restful.Api(app)
 status = 0 
 #0 stands for free status,1 for in Queue status and 2 for busy status
 
-global r
+global instance
 
 class HelloWorld(restful.Resource):
     def get(self):
@@ -29,8 +29,6 @@ class Inform(restful.Resource):
 class Job(restful.Resource):
     """ Takes the job , gets the task done and sends back the results"""
     def post(self):
-      #  get_job = request.get_json(force=True)
-      #  job = ast.literal_eval(get_job)
         job = request.get_json(force = True)
         if job :            
             status = 1
@@ -40,47 +38,44 @@ class Job(restful.Resource):
             url = job["url"] or "http://localhost:8080"
             num_workers = int(job["num_workers"])
             num_tasks = int(job["num_tasks"]) or num_workers * 100
-            global r
-            r = requests_store.Task(url, num_workers ,num_tasks)
-            result = r.start()
+            global instance
+            instance = requests_store.Task(url, num_workers ,num_tasks)
+            result = instance.start()
             final_report = {}
-            final_report["status"] = r.json_output_status()
-       #     final_report["time"] = r.json_output_timeseries()
+            final_report["status"] = instance.json_output_status()
+            final_report["time"] = instance.json_output_timeseries()
             final_report["port"] = sys.argv[3]
             requests.post("http://" + sys.argv[1] + ":" + sys.argv[2] + "/jobresult", data = json.dumps(final_report))      
             status = 0
             return 
         else:
-            return {'msg' : 'No job given'}
+            return {'Msg' : 'No job given'}
 
 class Stats(restful.Resource):
     def get(self):
         report = {}
         status_dic = {}
         try:
-            report["status"] = r.json_output_status()
+            report["status"] = instance.json_output_status()
+            report["job_status"] = instance.status
         except:
-            report["status"] = {"msg" : "Unable to get status"}
-#        try:
-#            status_dic = r.json_output_status()
-#        except:
-#            status_dic = {'msg' : 'Unable to get status'}
-#        for key in status_dic:
-#            report[key] = status_dic[key]
-      #  report["time"] = r.json_output_timeseries() 
-    #    requests.post("http://" + sys.argv[1] + ":" + sys.argv[2] + "/report", data = json.dumps(report))
-        return json.dumps(report)
+            report["status"] = json.dumps({"msg" : "No job running" })
+            report["job_status"] = False
+        try: 
+            report["time"] = instance.json_output_timeseries() 
+        except:
+            report["time"] = json.dumps({"msg" : "No time available" })
+        return report
 
 class Health(restful.Resource):
     """ Returns it's health condition""" 
     def get(self):
         return status
 
-#class Stop(restful.Resource):
-#    def get(self):
-#        ins = requests_store.Task("http://localhost:1500")
-#        ins.stop()
-#        return {'msg' : 'stopped'}
+class Stop(restful.Resource):
+    def get(self):
+        instance.stop()
+        return {'msg' : 'stopped'}
 
 @app.route('/shutdown', methods=['GET'])
 def shutdown():
@@ -100,7 +95,7 @@ api.add_resource(Inform, '/Inform')
 api.add_resource(Job, '/Job')
 api.add_resource(Health, '/Health')
 api.add_resource(Stats, '/Stats')
-#api.add_resource(Stop, '/Stop')
+api.add_resource(Stop, '/Stop')
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port=int(sys.argv[3]),threaded=True)
